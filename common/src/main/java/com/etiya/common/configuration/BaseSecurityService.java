62e10176fe2c2d824da8ab405ff20412d7e46bfc
mhmmedinan <minan.65@hotmail.com>
2024-08-15 15:59:52 +0300

    add security configuration

---
+++ common/src/main/java/com/etiya/common/configuration/BaseSecurityService.java
@@ -0,0 +1,34 @@
+package com.etiya.common.configuration;
+
+import com.etiya.common.filters.JwtAuthFilter;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BaseSecurityService {
+
+    private static final String[] WHITE_LIST_URLS = {
+            "/swagger-ui/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/api/v1/auth/**"
+    };
+
+    private  final JwtAuthFilter jwtAuthFilter;
+
+    public HttpSecurity configureCoreSecurity(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req->req.requestMatchers(WHITE_LIST_URLS).permitAll())
+                .addFilterBefore((Filter) jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity;
+
+    }
+
+}
