2ba541b5574b6efaf4bb51cbf3600cecba6bdcf1
mhmmedinan <minan.65@hotmail.com>
2024-08-16 14:15:00 +0300

    add productservice

---
+++ productservice/src/main/java/com/productservice/entities/Product.java
@@ -0,0 +1,32 @@
+package com.productservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "totalPrice")
+    private double totalPrice;
+}
